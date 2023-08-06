d06eddcc275d1c70d4197562469b087dd295c32e
Denis Silkov <Denis_Silkov@rntgroup.com>
2023-08-06 13:46:48 +0300

    Base page creation

--- section_4/test_main_page.py
+++ section_4/test_main_page.py
@@ -1,7 +1,15 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
+
+# Убедитесь, что тест работает, с помощью следующей команды: pytest -v --tb=line --language=en test_main_page.py.
+# Здесь и далее мы будем использовать эту команду для запуска. В этой команде мы использовали опцию PyTest --tb=line,
+# которая указывает, что нужно выводить только одну строку из лога каждого упавшего теста.
+# Так вам будет проще разобраться в том, как выглядят сообщения об ошибках.
